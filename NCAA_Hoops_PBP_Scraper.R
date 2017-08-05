6494e796383c981047dedfa00a43506cf39919d0
Luke Benz <luke.benz@yale.edu>
2017-08-04 23:29:45 -0400

    Data Scraper

---
+++ NCAA_Hoops_PBP_Scraper.R
@@ -0,0 +1,171 @@
+library(XML)
+library(dplyr)
+library(magrittr)
+
+### Function to clean PBP data
+clean <- function(data, half, OTs) {
+  cleaned <- data %>% mutate(play_id = 1:nrow(data), 
+                             half = half,
+                             time_remaining_half = as.character(V1), 
+                             description = V3, 
+                             away_score = suppressWarnings(as.numeric(gsub("-.*", "", V4))),
+                             home_score = suppressWarnings(as.numeric(gsub(".*-", "", V4))))
+  cleaned$time_remaining_half[1] <- ifelse(half <= 2, "20:00", "5:00")
+  mins <- suppressWarnings(as.numeric(gsub(":.*","", cleaned$time_remaining_half)))
+  secs <- suppressWarnings(as.numeric(gsub(".*:","", cleaned$time_remaining_half)))
+  cleaned$secs_remaining <- max(20 * (2 - half), 0) * 60 + 
+    5 * 60 * max((OTs * as.numeric(half <=2)), ((OTs + 2 - half) * as.numeric(half > 2))) + 60 * mins + secs
+  if(half == 1) {
+    cleaned[1, c("home_score", "away_score")] <- c(0,0)
+  }
+  cleaned <- select(cleaned, play_id, half, time_remaining_half, secs_remaining, description,
+                    home_score, away_score) 
+  return(cleaned)
+}
+
+### Create Team URL Dictionairy for Acquiring Team PBP Data
+teams_url <- "http://www.espn.com/mens-college-basketball/teams"
+x <- scan(teams_url, what = "", sep = "\n")
+x <- x[grep("http://www.espn.com/mens-college-basketball/team/_/id/", x)]
+x <- strsplit(x, "/")
+
+ids <- data.frame("team" = rep(NA, 351), 
+                  "id" = rep(NA, 351),
+                  "link" = rep(NA, 351))
+
+for(i in 1:length(x)) {
+  ids$id[i] <- x[[i]][8]
+  y <- gsub("[<>\"]", "" , x[[i]][9])
+  y <- gsub("class=bi", "",  y)
+  y <- unlist(strsplit(y, " "))
+  ids$link[i] <- y[1]
+  ids$team[i] <- paste(y[-1], collapse = " ")
+}
+
+get_pbp <- function(team) {
+  print(paste("Getting Game IDs: ", team, sep = ""))
+  ### Get Game IDs
+  base_url <- "http://www.espn.com/mens-college-basketball/team/_/id/"
+  url <- paste(base_url, ids$id[ids$team == team], "/", ids$link[ids$team == team], sep = "")
+  
+  x <- scan(url, what = "", sep = "\n")
+  x <- x[grep("club-schedule", x)]
+  x <- unlist(strsplit(x, "gameId="))
+  x <- x[-1]
+  x <- x[1:(floor(length(x)/2))]
+  
+  gameIDs <- substring(x, 1, 9)
+  gameIDs <- unique(gameIDs)
+  
+  ### Get Play by Play Data 
+  base_url <- "http://www.espn.com/mens-college-basketball/playbyplay?gameId="
+  summary_url <- "http://www.espn.com/mens-college-basketball/game?gameId="
+  j <- 0
+  
+  for(i in 1:length(gameIDs)) {
+    print(paste("Getting ", team, " Game: ", i, "/", length(gameIDs), sep = ""))
+    url <- paste(base_url, gameIDs[i], sep = "")
+    tmp <- try(readHTMLTable(url), silent = T)
+    
+    ### Check if PBP Data is Available
+    if(length(tmp) < ncol(tmp[[1]])) {
+      print("Play by Play Data Not Available")
+      next
+    }
+    else{
+      j <- j + 1
+    }
+    
+    
+    ### 0 OT
+    if(ncol(tmp[[1]]) == 4) {
+      half_1 <- clean(as.data.frame(tmp[[2]]), 1, 0)
+      half_2 <- clean(as.data.frame(tmp[[3]]), 2, 0)
+      pbp <- rbind(half_1, half_2)
+    }
+    
+    ### 1 OT
+    if(ncol(tmp[[1]]) == 5) {
+      half_1 <- clean(as.data.frame(tmp[[2]]), 1, 1)
+      half_2 <- clean(as.data.frame(tmp[[3]]), 2, 1)
+      half_3 <- clean(as.data.frame(tmp[[4]]), 3, 1)
+      pbp <- rbind(half_1, half_2, half_3)
+    }
+    
+    ### 2 OT
+    if(ncol(tmp[[1]]) == 6) {
+      half_1 <- clean(as.data.frame(tmp[[2]]), 1, 2)
+      half_2 <- clean(as.data.frame(tmp[[3]]), 2, 2)
+      half_3 <- clean(as.data.frame(tmp[[4]]), 3, 2)
+      half_4 <- clean(as.data.frame(tmp[[5]]), 4, 2)
+      pbp <- rbind(half_1, half_2, half_3, half_4)
+    }
+    
+    ### 3 OT
+    if(ncol(tmp[[1]]) == 7) {
+      half_1 <- clean(as.data.frame(tmp[[2]]), 1, 3)
+      half_2 <- clean(as.data.frame(tmp[[3]]), 2, 3)
+      half_3 <- clean(as.data.frame(tmp[[4]]), 3, 3)
+      half_4 <- clean(as.data.frame(tmp[[5]]), 4, 3)
+      half_5 <- clean(as.data.frame(tmp[[6]]), 5, 3)
+      pbp <- rbind(half_1, half_2, half_3, half_4, half_5)
+    }
+    
+    ### 4 OT
+    if(ncol(tmp[[1]]) == 8) {
+      half_1 <- clean(as.data.frame(tmp[[2]]), 1, 4)
+      half_2 <- clean(as.data.frame(tmp[[3]]), 2, 4)
+      half_3 <- clean(as.data.frame(tmp[[4]]), 3, 4)
+      half_4 <- clean(as.data.frame(tmp[[5]]), 4, 4)
+      half_5 <- clean(as.data.frame(tmp[[6]]), 5, 4)
+      half_6 <- clean(as.data.frame(tmp[[7]]), 5, 4)
+      pbp <- rbind(half_1, half_2, half_3, half_4, half_6)
+    }
+    
+    these <- grep(T, is.na(pbp$home_score))
+    pbp[these, c("home_score", "away_score")] <- pbp[these - 1 , c("home_score", "away_score")]
+    
+    ### Get full team names 
+    url2 <- paste(summary_url, gameIDs[i], sep = "")
+    tmp <- readHTMLTable(url2)
+    pbp$away <- as.character(as.data.frame(tmp[[2]])[1,1])
+    pbp$home <- as.character(as.data.frame(tmp[[2]])[2,1])
+    away_abv <- as.character(as.data.frame(tmp[[1]])[1,1])
+    home_abv <- as.character(as.data.frame(tmp[[1]])[2,1])
+    
+    ### Get Game Line
+    y <- scan(url2, what = "", sep = "\n")
+    y <- y[grep("Line:", y)]
+    if(length(y) > 0) {
+      y <- gsub("<[^<>]*>", "", y)
+      y <- gsub("\t", "", y)
+      y <- strsplit(y, ": ")[[1]][2]
+      line <- as.numeric(strsplit(y, " ")[[1]][2])
+      abv <- strsplit(y, " ")[[1]][1]
+      if(abv == home_abv) {
+        line <- line * -1
+      }
+    }
+    else {
+      line <- NA
+    }
+    
+    pbp$home_favored_by <- line
+    pbp$play_id <- 1:nrow(pbp)
+    pbp$game_id <- gameIDs[i]
+    
+    if(j == 1) {
+      pbp_season <- pbp
+    }else{
+      pbp_season <- rbind(pbp_season, pbp)
+    }
+  }
+  return(pbp_season)
+}
+
+### Get all of 2016/17 Data
+for(k in 1:351) {
+  data <- get_pbp(ids$team[k])
+  write.table(data, paste("pbp_2016_17/", gsub(" ", "_", ids$team[k]), ".csv", sep = ""), row.names = F, col.names = T, sep = ",")
+}
+
