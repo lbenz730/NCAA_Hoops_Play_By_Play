ce634a0ba7b7a5b52ca88f7d0b6770227a27c40a
Luke Benz <luke.benz@yale.edu>
2017-12-20 11:26:56 -0500

    Added Centrality Measures

--- Assist_Networks.R
+++ Assist_Networks.R
@@ -1,10 +1,11 @@
 ### NCAA Assist Networks
 ### Luke Benz
-### Version 1.4 (Updated 12.16.17)
+### Version 2.0 (Updated 12.19.17)
 
 library(igraph)
+library(dplyr)
 
-assist_net <- function(team, node_col, season, rmv_bench, tree) {
+assist_net <- function(team, node_col, season, rmv_bench, tree, three_weights) {
   ### Read Play-by-Play File
   if(season[1] == "2016-17") {
     x <- read.csv(paste("pbp_2016_17/", team, ".csv", sep = ""), as.is = T)
@@ -55,6 +56,15 @@ assist_net <- function(team, node_col, season, rmv_bench, tree) {
   network <- data.frame("ast" = rep(NA, sets), 
                         "shot" = rep(NA, sets),
                         "num" = rep(NA, sets))
+  
+  ### Adjust Three Point Weights in Network
+  x$weights <- 1
+  if(three_weights){
+    threes <- grep("Three Point", x$description)
+    x$weights[threes] <- 1.5
+  }
+  
+  ### Aggregate Assists
   for(i in 1:nrow(roster)) {
     ast <- roster$Name[i]
     tmp <- roster[roster$Name != ast,]
@@ -62,7 +72,7 @@ assist_net <- function(team, node_col, season, rmv_bench, tree) {
       index <- j + (i - 1) * nrow(tmp)
       network$ast[index] <- ast
       network$shot[index] <- tmp$Name[j]
-      network$num[index] <- sum(x$ast == ast & x$shot == tmp$Name[j])
+      network$num[index] <- sum(x$weights[x$ast == ast & x$shot == tmp$Name[j]])
     }
   }
   
@@ -77,7 +87,25 @@ assist_net <- function(team, node_col, season, rmv_bench, tree) {
   ast_data <- aggregate(a_freq ~ ast, data = network, sum)
   shot_data <- aggregate(a_freq ~ shot, data = network, sum)
   
-  ### Create Network
+  ### Create Temporary Directed Network 
+  net <- graph.data.frame(network, directed = T)
+  deg <- degree(net, mode="all")
+  E(net)$weight <- network$num
+  
+  ### Compute Clustering Coefficient
+  clust_coeff <- round(transitivity(net, type = "global"), 3)
+  
+  ### Compute Page Rank
+  pagerank <- sort(page_rank(net)$vector, decreasing = T)
+  
+  ### Compute Hub Score
+  hubscores <- sort(hub_score(net, scale = F)$vector, decreasing = T)
+  
+  ### Compute Authority Scores
+  auth_scores <- sort(authority_score(net, scale = F)$vector, decreasing = T)
+  
+  
+  ### Create/Plot Undirected Network
   net <- graph.data.frame(network, directed = F)
   deg <- degree(net, mode="all")
   E(net)$weight <- network$num
@@ -86,14 +114,37 @@ assist_net <- function(team, node_col, season, rmv_bench, tree) {
   E(net)$width <- E(net)$weight * factor
   V(net)$color <- node_col
   
-  ### Plot Network
   plot(net, vertex.label.color= "black", vertex.label.cex = 0.5,
        layout= ifelse(tree, layout_as_tree,layout_in_circle),
-       vertex.label.family = "Arial Black", main = paste(team, text, sep = ""))  
+       vertex.label.family = "Arial Black", 
+       main = paste(team, ifelse(three_weights, " Weighted", ""), text, sep = ""))  
   
-  ### Compute and Return Clustering Coefficient
-  clust_coeff <- round(transitivity(net, type = "global"), 3)
-  print(paste("Clustering Coefficient: ", clust_coeff))
-  return(clust_coeff)
+  ### Add Text to Network
+  text(-1.5, 1.0, paste(ifelse(three_weights, "Weighted ", ""), "Assist Frequency Leader: ", 
+                        ast_data$ast[which.max(ast_data$a_freq)], " (", 
+                        round(100 * max(ast_data$a_freq), 1), "%)", sep = ""), 
+       cex = ifelse(three_weights, 0.8, 0.6))
+  text(-1.5, 0.9, paste(ifelse(three_weights, "Weighted ", ""), "(Assisted) Shot Frequency Leader: ", 
+                        shot_data$shot[which.max(shot_data$a_freq)], " (", 
+                        round(100 * max(shot_data$a_freq), 1), "%)", sep = ""), 
+       cex = ifelse(three_weights, 0.8, 0.6))
+  text(-1.5, 0.8, paste("PageRank MVP: ", names(which.max(pagerank)), " (", 
+                        round(max(pagerank), 3), ")", sep = ""))
+  text(-1.5, 0.7, paste("Hub Score MVP: ", names(which.max(hubscores)), " (", 
+                        round(max(hubscores), 3), ")", sep = ""))
+  text(-1.5, 0.6, paste("Authority Score MVP: ", names(which.max(auth_scores)), " (", 
+                        round(max(auth_scores), 3), ")", sep = ""))
+  text(-1.5, 0.5, paste("Team Clustering Coefficient: ", clust_coeff, sep = ""))
+  
+  if(three_weights){
+    text(0, -1.4, cex = 0.7,
+         paste("Weighted Assist Network: Assisted 2 point shots are given weight 1, ", 
+               "Assisted 3 point shots are given weight 1.5", sep = ""))
+  }
+  
+  
+  ### Return Results
+  return(list("clust_coeff" = clust_coeff, "page_ranks" = pagerank, 
+              "hub_scores" = hubscores, "auth_scores" = auth_scores))
 }
 
