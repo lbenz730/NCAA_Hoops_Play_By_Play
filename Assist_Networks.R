2ed455c3cc7d70e6173a6ec39117cb53f8ad4822
Luke Benz <luke.benz@yale.edu>
2018-01-04 11:11:06 -0500

    M. Bagley III Fix

--- Assist_Networks.R
+++ Assist_Networks.R
@@ -1,6 +1,6 @@
 ### NCAA Assist Networks
 ### Luke Benz
-### Version 2.2.0 (Updated 1.1.18)
+### Version 2.2.1 (Updated 1.4.18)
 
 library(igraph)
 library(dplyr)
@@ -38,6 +38,7 @@ assist_net <- function(team, node_col, season, rmv_bench, tree, three_weights) {
   team <- gsub(" ", "_", team)
   roster <- read.csv(paste("rosters_2017_18/", team, ".csv", sep = ""), as.is = T)
   roster$Name <- gsub(" Jr.", "", roster$Name)
+  roster$Name <- gsub(" III", "", roster$Name)
   games <- unique(x$game_id)
   ast <- grep("Assisted", x$description)
   x <- x[ast, ]
